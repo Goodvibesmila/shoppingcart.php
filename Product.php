83480393fe98fcdd6aee0ca8c715ea51fecf1a77
Emilia Johansson <johansson.emilia@hotmail.se>
2022-11-27 17:26:36 +0100

    basic code in product class

---
+++ Product.php
@@ -0,0 +1,34 @@
+<?php
+
+//HELA DENNA SIDAN
+
+class Product
+{
+
+    public function __construct($id, $title, $price, $inStock)
+    
+    {
+        $this->id = $id;
+        $this->title = $title;
+        $this->price = $price;
+        $this->inStock = $inStock; 
+    }
+
+    private $id;
+    private $title;
+    private $price;
+    private $inStock;
+
+    public function getProduct()
+    {
+        return $this->id . " " . $this->title . " " . $this->price . " " . $this->inStock;
+    }
+
+    // CHECK TODO Skriv en konstruktor som sätter alla properties
+
+    // CHECK TODO Skriv getters för alla properties
+
+}
+
+// Överflödig? $product1 = new order();
+// Behöver vi skriva följande kod: echo $product1->getProduct(); 
